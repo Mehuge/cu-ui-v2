c353c1078777e0ede312e1aac7304c5f1d7dadd5
Mehuge <adf@sorcerer.co.uk>
2015-08-27 21:42:28 +0100

    Moved resources publish to parent gulpfile, and added build.cmd to automate all the glue

--- enemytarget/gulpfile.js
+++ enemytarget/gulpfile.js
@@ -7,20 +7,9 @@
 'use strict';
 
 var gulp = require('gulp');
-var debug = require('gulp-debug');
 var buildConfig = require('./cu-build.config.js');
 var buildTools = require('cu-build-tools');
 
-gulp.task('resources', function() {
-	console.log('running publish:after');
-	gulp.src([ 
-		'node_modules/cu-components/lib/**/*.png',
-		'node_modules/cu-components/lib/**/*.jpg',
-		'node_modules/cu-components/lib/**/*.css'
-	])
-	.pipe(debug('title', 'copy:'))
-	.pipe(gulp.dest('../publish/lib/cu-components'));
-});
 
 // load build tool tasks and obtain processed build configuration
 var config = buildTools.auto(gulp, buildConfig);
