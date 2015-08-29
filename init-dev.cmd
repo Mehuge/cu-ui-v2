1b4b28c17933f16bb8ea3dd92ad2b395cf16caa1
Mehuge <adf@sorcerer.co.uk>
2015-08-29 19:25:01 +0100

    Initialise the development environment (run after cloning)

---
+++ init-dev.cmd
@@ -0,0 +1,12 @@
+npm install
+cd character
+npm install ; tsd install
+cd ..\enemytarget
+npm install ; tsd install
+cd ..\friendlytarget
+npm install ; tsd install
+cd ..\lib
+git clone https://github.com/Mehuge/cu-components
+cd cu-components
+npm install ; tsd install
+cd ../..
