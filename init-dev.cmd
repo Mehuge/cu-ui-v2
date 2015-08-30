ca56c6ca0dfc9f5fd6a363a2e0760860a200bc8a
saddieeiddas <saddie.eiddas@gmail.com>
2015-08-30 01:04:12 +0100

    update init-dev script

--- init-dev.cmd
+++ init-dev.cmd
@@ -1,12 +1,8 @@
-npm install
-cd character
-npm install ; tsd install
-cd ..\enemytarget
-npm install ; tsd install
-cd ..\friendlytarget
-npm install ; tsd install
-cd ..\lib
-git clone https://github.com/Mehuge/cu-components
-cd cu-components
-npm install ; tsd install
-cd ../..
+:: install development packages
+call npm install
+
+:: clone libraries
+git clone https://github.com/Mehuge/cu-components lib/cu-components
+
+:: install all module and library dependencies/definitions
+gulp install
