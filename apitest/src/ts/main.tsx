04b1fc72ca3017a5f187a625c9ceb908280839d2
Mehuge <adf@sorcerer.co.uk>
2015-08-30 23:45:21 +0100

    Add a bunch of tests

--- apitest/src/ts/main.tsx
+++ apitest/src/ts/main.tsx
@@ -2,56 +2,140 @@
 import * as React from 'react';
 import * as Reflux from 'reflux';
 import events from 'cu-events';
+
+// Test: ControlGameScoreStore
 import { ControlGameScoreStore } from 'cu-stores';
-
 const controlGameScoreStore = ControlGameScoreStore.create();
+const TestControlGameScoreStore = React.createClass({
+    mixins: [Reflux.connect(controlGameScoreStore, 'score')],
+    getInitialState: function() {
+        return { score: controlGameScoreStore.info };
+    },
+    componentDidMount() {
+        controlGameScoreStore.start(); // FIXME! events.handlesControlGameScore.start();
+    },
+    render: function() {
+        return (<div><h1>ControlGameScoreStore</h1><p>{ JSON.stringify(this.state.score) }</p></div>);
+    }
+});
 
-const ApiTest = React.createClass({
-
-	// Hook store up to component.  Each time character data is changed,
-	// our state is updated, triggering a render
-	mixins: [
-		Reflux.connect(controlGameScoreStore, 'score')
-	],
-
-	// Provide an initial state (TODO: is there a better way to do this?)
+// Test: ControlGameStore
+import { ControlGameStore } from 'cu-stores';
+const controlGameStore = ControlGameStore.create();
+const TestControlGameStore = React.createClass({
+	mixins: [Reflux.connect(controlGameStore, 'game')],
 	getInitialState: function() {
-		return { score: controlGameScoreStore.info };
+		return { game: controlGameStore.info };
 	},
-
 	componentDidMount() {
-		console.log('ApiTest: handlesControlGameStart.start()');
-		// BROKE! events.handlesControlGameScore.start();
-		controlGameScoreStore.start();
+		controlGameStore.start(); // FIXME! events.handlesControlGame.start();
 	},
-
-	// Render the unit frame using character data
 	render: function() {
-		return (
-			<div>{ JSON.stringify(this.state.score) }</div>
-		);
+		return (<div><h1>ControlGameStore</h1><p>{ JSON.stringify(this.state.game) }</p></div>);
 	}
 });
 
-events.on("init", function() {
-	React.render(<ApiTest/>, document.getElementById("cse-ui-apitest"));
+// Test: TestCharacterStore
+import { CharacterStore } from 'cu-stores';
+const characterStore = CharacterStore.create();
+const TestCharacterStore = React.createClass({
+    mixins: [Reflux.connect(characterStore, 'game')],
+    getInitialState: function() {
+        return { game: characterStore.info };
+    },
+    componentDidMount() {
+        characterStore.start(); // FIXME! events.handlesCharacter.start();
+    },
+    render: function() {
+        return (<div><h1>CharacterStore</h1><p>{ JSON.stringify(this.state.game) }</p></div>);
+    }
+});
 
-	// Basic Reflux Action test.
+// Test: TestEnemyTargetStore
+import { EnemyTargetStore } from 'cu-stores';
+const enemyTargetStore = EnemyTargetStore.create();
+const TestEnemyTargetStore = React.createClass({
+    mixins: [Reflux.connect(enemyTargetStore, 'enemyTarget')],
+    getInitialState: function() {
+        return { enemyTarget: enemyTargetStore.info };
+    },
+    componentDidMount() {
+        enemyTargetStore.start(); // FIXME! events.handlesEnemyTarget.start();
+    },
+    render: function() {
+        return (<div><h1>EnemyTargetStore</h1><p>{ JSON.stringify(this.state.enemyTarget) }</p></div>);
+    }
+});
+
+// Test: TestFriendlyTargetStore
+import { FriendlyTargetStore } from 'cu-stores';
+const friendlyTargetStore = FriendlyTargetStore.create();
+const TestFriendlyTargetStore = React.createClass({
+    mixins: [Reflux.connect(friendlyTargetStore, 'friendlyTarget')],
+    getInitialState: function() {
+        return { friendlyTarget: friendlyTargetStore.info };
+    },
+    componentDidMount() {
+        friendlyTargetStore.start(); // FIXME! events.handlesFriendlyTarget.start();
+    },
+    render: function() {
+        return (<div><h1>FriendlyTargetStore</h1><p>{ JSON.stringify(this.state.friendlyTarget) }</p></div>);
+    }
+});
+
+// Test: TestAnnouncementStore
+import { AnnouncementsStore } from 'cu-stores';
+const announcementsStore = AnnouncementsStore.create();
+const TestAnnouncementsStore = React.createClass({
+    mixins: [Reflux.connect(announcementsStore, 'announcements')],
+    getInitialState: function() {
+        return { announcements: announcementsStore.info };
+    },
+    componentDidMount() {
+        announcementsStore.start(); // FIXME! events.handlesAnnouncements.start();
+    },
+    render: function() {
+        return (<div><h1>AnnouncementsStore</h1><p>{ JSON.stringify(this.state.announcements) }</p></div>);
+    }
+});
+
+const Tests = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <TestControlGameScoreStore/>
+                <TestControlGameStore/>
+                <TestCharacterStore/>
+                <TestEnemyTargetStore/>
+                <TestFriendlyTargetStore/>
+                <TestAnnouncementsStore/>
+            </div>
+        );
+    }
+});
+
+events.on("init", function() {
+
+	// Render the UI
+	React.render(<Tests/>, document.getElementById("cse-ui-apitest"));
+
+	// Basic Reflux Action test (this is working)
 	const action : any = Reflux.createActions(["test"]);
 	console.dir(action);
 	console.dir(action.test);
+	console.dir(action.test.shouldEmit());
 	const store : any = Reflux.createStore({
 		listenables: action,
 		test() {
 			console.log('test action fired');
-			debugger;
 		}
 	});
 	console.dir(store);
 	action.test();
 
-
+	console.log('handlesControlGameScore action:-');
 	console.dir(events.handlesControlGameScore.action);
+	console.dir(events.handlesControlGameScore.action.start);
+	console.dir(events.handlesControlGameScore.action.start.shouldEmit());
 	events.handlesControlGameScore.action.start();
-
 });
