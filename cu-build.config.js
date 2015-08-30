2ec1b037cd321f64463a87d2ab65e88fa179cab6
saddieeiddas <saddie.eiddas@gmail.com>
2015-08-30 20:25:59 +0100

    update server injected scripts

--- cu-build.config.js
+++ cu-build.config.js
@@ -14,4 +14,11 @@ module.exports = {
   build: {
     ui_nested: false,
   },
+  server: {
+    inject: {
+      scripts_before: [
+        __dirname + '/node_modules/cu-fake-api/lib/main.js'
+      ],
+    },
+  },
 };
